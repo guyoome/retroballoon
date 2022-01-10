cd309034148ec8408538bc6aacb84cac07d20edc
Guyoome <guillaumemaurin.gm@gmail.com>
2022-01-10 23:14:00 +0100

    Add zoom

--- src/components/WhiteBoard.jsx
+++ src/components/WhiteBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Stage, Layer } from "react-konva";
 import { StickyNote } from "./StickyNote";
 import COLORS from "../utils/colors.json";
@@ -6,11 +6,54 @@ import COLORS from "../utils/colors.json";
 const WhiteBoard = () => {
   const [selected, setSelected] = useState();
   const [stickyNotes, setStickyNotes] = useState([]);
+  const [stageScale, setStageScale] = useState(1);
+  const [stageX, setStageX] = useState(0);
+  const [stageY, setStageY] = useState(0);
+  const [handleWheel, setHandleWheel] = useState();
+
+  useEffect(() => {
+    try {
+      handleWheel.evt.preventDefault();
+  
+      // const scaleBy = 1.01;
+      const scaleBy = 1.05;
+
+      const stage = handleWheel.target.getStage();
+      const oldScale = stage.scaleX();
+      const mousePointTo = {
+        x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+        y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+      };
+  
+      const newScale = handleWheel.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+  
+      stage.scale({ x: newScale, y: newScale });
+  
+      // this.setState({
+      //   stageScale: newScale,
+      //   stageX:
+      //     -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale,
+      //   stageY:
+      //     -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale
+      // });
+      setStageScale(newScale);
+      setStageX(-(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale);
+      setStageY(-(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale);
+      
+    } catch (error) {
+      
+    }
+  }, [handleWheel]);
 
   return (
     <Stage
       width={window.innerWidth}
       height={window.innerHeight}
+      scaleX={stageScale}
+      scaleY={stageScale}
+      x={stageX}
+      y={stageY}
+      onWheel={(e) => setHandleWheel(e)}
       onClick={(e) => {
         if (e.currentTarget._id === e.target._id) {
           setSelected();
@@ -19,6 +62,7 @@ const WhiteBoard = () => {
         }
       }}
       onDblClick={(e) => {
+        console.log("🅱️",e)
         if (e.currentTarget._id === e.target._id) {
           setStickyNotes(stickyNotes.concat({
             x: e.evt.layerX,
