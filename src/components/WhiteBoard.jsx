c448cd7ff59057b92c62998cea16d3e801f29f03
Guyoome <guillaumemaurin.gm@gmail.com>
2022-01-13 21:15:24 +0100

    Echec

--- src/components/WhiteBoard.jsx
+++ src/components/WhiteBoard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Rect, Group } from "react-konva";
 import { StickyNote } from "./StickyNote";
 import COLORS from "../utils/colors.json";
 
@@ -14,16 +14,16 @@ const WhiteBoard = ({ onZoom, scale, stageX, stageY }) => {
       style={{ backgroundColor: "#eff3f6", cursor: cursor }}
       width={window.innerWidth}
       height={window.innerHeight}
-      draggable
-      scaleX={scale}
-      scaleY={scale}
-      x={stageX}
-      y={stageY}
+      // draggable
+      // scaleX={scale}
+      // scaleY={scale}
+      // x={stageX}
+      // y={stageY}
       onWheel={(e) => onZoom(e)}
-      
+
       // onTouchStart={()=>{console.log("hey")}}
       // onPointerMove={(e)=>{console.log("hey",e)}}
-      onDragStart={(e) => { if (e.currentTarget._id === e.target._id) { setCursor("grabbing");console.log("👻e",e) } }}
+      onDragStart={(e) => { if (e.currentTarget._id === e.target._id) { setCursor("grabbing"); console.log("👻e", e) } }}
       onDragEnd={(e) => setCursor("default")}
       onMouseDown={(e) => { if (e.currentTarget._id === e.target._id) { setCursor("grabbing") } }}
       onMouseUp={(e) => setCursor("default")}
@@ -34,29 +34,53 @@ const WhiteBoard = ({ onZoom, scale, stageX, stageY }) => {
           setSelected(e.target.parent.attrs.id);
         }
       }}
-      onDblClick={(e) => {
-        if (e.currentTarget._id === e.target._id) {
-          const mouse = e.target.getRelativePointerPosition();
-          setStickyNotes(stickyNotes.concat({
-            x: mouse.x,
-            y: mouse.y
-          }))
-          setSelected(`SN-${stickyNotes.length}`);
-        }
-      }}>
-      <Layer>
-        {stickyNotes.map((stickyNote, id) => (
-          <StickyNote
-            key={id}
-            id={`SN-${id}`}
-            x={stickyNote.x}
-            y={stickyNote.y}
-            color={COLORS.blue}
-            selected={selected === `SN-${id}` ? true : false}
-          />
-        )
-        )}
-      </Layer>
+      // onDblClick={(e) => {
+      //   if (e.currentTarget._id === e.target._id) {
+      //     const mouse = e.target.getRelativePointerPosition();
+      //     setStickyNotes(stickyNotes.concat({
+      //       x: mouse.x,
+      //       y: mouse.y
+      //     }))
+      //     setSelected(`SN-${stickyNotes.length}`);
+      //   }
+      // }}
+      >
+      {/* <Group 
+      scaleX={scale}
+      scaleY={scale}
+      x={stageX}
+      y={stageY}> */}
+        <Layer
+        scaleX={scale}
+        scaleY={scale}
+        x={stageX}
+        y={stageY}
+        width={1000}
+        height={1000}
+        onDblClick={(e) => {
+          console.log("ksdlskq")
+          if (e.currentTarget._id === e.target._id) {
+            const mouse = e.target.getRelativePointerPosition();
+            setStickyNotes(stickyNotes.concat({
+              x: mouse.x,
+              y: mouse.y
+            }))
+            setSelected(`SN-${stickyNotes.length}`);
+          }
+        }}> 
+          {stickyNotes.map((stickyNote, id) => (
+            <StickyNote
+              key={id}
+              id={`SN-${id}`}
+              x={stickyNote.x}
+              y={stickyNote.y}
+              color={COLORS.blue}
+              selected={selected === `SN-${id}` ? true : false}
+            />
+          )
+          )}
+        </Layer>
+      {/* </Group> */}
     </Stage>
   );
 };
